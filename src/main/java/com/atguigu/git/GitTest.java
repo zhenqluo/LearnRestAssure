95a1dcb2124170f6186fbb5eb333b9a6019cb82a
zhenqluo <luozq11@163.com>
2022-03-11 23:30:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+/**
+ * Created by wuyuanyan on 2022/3/11.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
